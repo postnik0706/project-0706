fa82c1397178b971b17242529d38131e6e8e6b53
postnik0706 <postnik0706@0245cc7d-1c5a-0d63-4fdc-0e8d0fe1a5ef>
2011-09-25 21:27:05 +0000

    After video # 3 - filtering products

---
+++ RegMaster/RegMaster.Data/Filters/ProductFilters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RegMaster.Data
+{
+    public static class ProductFilters
+    {
+        /// <summary>
+        /// Filters the product by category ID
+        /// </summary>
+        /// <param name="qry"></param>
+        /// <param name="categoryID"></param>
+        /// <returns></returns>
+        public static IQueryable<Product> WithCategory(
+            this IQueryable<Product> qry, int categoryID)
+        {
+            return from p in qry
+                   where p.CategoryID == categoryID
+                   select p;
+        }
+
+        public static IQueryable<Product> WithID(
+            this IQueryable<Product> qry,
+            int ProductID)
+        {
+            return from p in qry
+                   where p.ID == ProductID
+                   select p;
+        }
+    }
+}
--- RegMaster/RegMaster.Tests/TestCatalogRepository.cs
+++ RegMaster/RegMaster.Tests/TestCatalogRepository.cs
@@ -13,21 +13,50 @@ namespace RegMaster.Tests
         public IQueryable<Data.Category> GetCategories()
         {
             IList<Category> result = new List<Category>();
+
             for (int i = 0; i < 2; i++)
             {
                 Category cat = new Category();
-                cat.ID = i;
+                cat.ID = i * 6;
                 cat.Name = String.Format("Parent {0}", i);
                 cat.ParentID = null;
                 result.Add(cat);
 
-                for (int j = 0; j < 5; j++)
+                for (int j = 1; j <= 5; j++)
                 {
                     Category sub = new Category();
-                    sub.ID = j;
+                    sub.ID = cat.ID + j;
                     sub.ParentID = cat.ID;
                     sub.Name = String.Format("Sub {0}", j);
                     result.Add(sub);
+                }
+            }
+
+            return result.AsQueryable();
+        }
+
+        public IQueryable<Product> GetProducts()
+        {
+            IList<Product> result = new List<Product>();
+            int uniqueProductID = 1;
+
+            var cat = GetCategories()
+                .Where(x => x.ParentID.HasValue)
+                .ToList();
+
+            foreach (Category c in cat)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Product p = new Product();
+                    p.Name = String.Format("Product {0}", i);
+                    p.ID = uniqueProductID;
+                    p.ListPrice = 5.68M;
+                    p.Description = "Test Description";
+
+                    p.CategoryID = c.ID;
+                    uniqueProductID++;
+                    result.Add(p);
                 }
             }
 
