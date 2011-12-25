7d9e4566b82ca80c17960a613ea62e8566c46f22
postnik0706 <postnik0706@0245cc7d-1c5a-0d63-4fdc-0e8d0fe1a5ef>
2011-12-25 23:30:55 +0000

    optimization

--- trunk/alien_numbers/alien_numbers/Program.cs
+++ trunk/alien_numbers/alien_numbers/Program.cs
@@ -10,7 +10,7 @@ namespace alien_numbers
     {
         static void Main(string[] args)
         {
-            string[] inp = File.ReadAllLines(".\\A-large-practice.in");
+            string[] inp = File.ReadAllLines(".\\simple.txt");
             short num_cases = short.Parse(inp[0]);
             System.Diagnostics.Debug.Assert(num_cases > 0, "Num cases > 0");
 
@@ -18,43 +18,22 @@ namespace alien_numbers
             for (int i = 0; i < num_cases; i++)
             {
                 string[] record = inp[i + 1].Split(' ');
-                res[i] = String.Format("Case #{0}: {1}",
-                    i + 1,
-                    dec_to_target(
-                    source_to_dec(record[0], record[1]),
-                    record[2]));
+
+                int x = 0;
+                for (int j = 0; j < record[0].Length; j++)
+                {
+                    x *= record[1].Length;
+                    x += record[1].IndexOf(record[0][j]);
+                }
+
+                res[i] = "";
+                while (x > 0)
+                {
+                    res[i] = record[2][x % record[2].Length] + res[i];
+                    x /= record[2].Length;
+                };
             }
             File.WriteAllLines(".\\out.txt", res);
-        }
-
-        static int source_to_dec(string alien_number, string source_language)
-        {
-            int n_alien_number = 0;
-            for (int i = alien_number.Length-1; i >= 0 ; i--)
-                n_alien_number += source_language.IndexOf(alien_number[alien_number.Length - i - 1])
-                    * (int)(Math.Pow(source_language.Length, i));
-            return n_alien_number;
-        }
-
-        static string dec_to_target(int dec, string target_language)
-        {
-            string source_language = "0123456789";
-            string res = "";
-            
-            int num_digits = 1;
-            while ((int)(dec / Math.Pow((double)target_language.Length, num_digits)) > 0)
-                num_digits++;
-
-            for (int j = num_digits - 1; j >= 0; j--)
-            {
-                int digit = (int)
-                    (dec /
-                    System.Math.Pow(target_language.Length, j));
-                res += target_language[digit];
-                dec = (int)(dec %
-                    System.Math.Pow(target_language.Length, j));
-            };
-            return res;
         }
     }
 }
