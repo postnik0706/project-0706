b8e1f9b6aad6426ebf9e587ad08468a0686c33c6
postnik0706 <postnik0706@0245cc7d-1c5a-0d63-4fdc-0e8d0fe1a5ef>
2012-03-18 17:24:47 +0000

    Adding Contact and CallLog classes, test data

---
+++ trunk/app_3/test_app3/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Reflection;
+
+namespace test_app3
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ReadAndGenerateTestClasses()
+        {
+            File.WriteAllText("out.txt", "This is a test");
+
+            Console.WriteLine(Assembly.GetCallingAssembly().Location);
+            //string[] lines = File.ReadAllLines(@"..\Input.txt");
+            //Console.WriteLine(lines.Count());
+        }
+    }
+}
