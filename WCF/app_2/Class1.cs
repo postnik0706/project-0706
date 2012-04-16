5216a1fd3731a5cfccaee9b9ff619745cb9abdba
postnik0706 <postnik0706@0245cc7d-1c5a-0d63-4fdc-0e8d0fe1a5ef>
2012-04-16 05:47:42 +0000

    initial check-in

---
+++ WCF/app_2/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace My.Service.Contracts
+{
+    [ServiceContract()]
+    public interface ICarRentalService
+	{
+        [OperationContract()]
+        double CalculatePrice(DateTime pickupDate, DateTime returnDate, string pickupLocation, string carType);
+	}
+}
