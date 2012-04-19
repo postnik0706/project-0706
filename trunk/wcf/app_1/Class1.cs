a55cfb6da76aee992f66ae075cfd31e2af49f99f
postnik0706 <postnik0706@0245cc7d-1c5a-0d63-4fdc-0e8d0fe1a5ef>
2012-04-19 10:06:36 +0000

    server

--- trunk/wcf/app_1/Class1.cs
+++ trunk/wcf/app_1/Class1.cs
@@ -2,11 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ServiceModel;
 
 namespace app_1
 {
     public class Class1
     {
-        
+        [ServiceContract()]
+        public interface ICarRentalService
+        {
+            [OperationContract()]
+            double CalculatePrice(DateTime pickupDate, DateTime returnDate, string pickupLocation,
+                string vehiclePreference);
+        }
+
+        public class CarRentalService : ICarRentalService
+        {
+            public double CalculatePrice(DateTime pickupDate, DateTime returnDate, string pickupLocation,
+                string vehiclePreference)
+            {
+                Random r = new Random(DateTime.Now.Millisecond);
+                return r.NextDouble() * 500;
+            }
+        }
     }
 }
