dbb2fe14d79b73bec46ad87b010606495fe9a5f6
postnik0706 <postnik0706@0245cc7d-1c5a-0d63-4fdc-0e8d0fe1a5ef>
2012-05-15 04:35:10 +0000

    Getting top 100 names from Wikipedia - proof

--- trunk/RandomDataGenerator/RandomDataGenerator/Program.cs
+++ trunk/RandomDataGenerator/RandomDataGenerator/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using HtmlAgilityPack;
 
 namespace RandomDataGenerator
 {
@@ -9,6 +10,16 @@ namespace RandomDataGenerator
     {
         static void Main(string[] args)
         {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web
+                .Load("http://en.wikipedia.org/wiki/List_of_most_common_surnames_in_North_America");
+
+            var tags = doc.DocumentNode.SelectNodes("/html[1]/body[1]/div[3]/div[3]/div[4]/table[4]/tr/td[1]");
+
+            foreach (var item in tags)
+            {
+                Console.WriteLine(item.InnerText);
+            }
         }
     }
 }
