2219ca3d810944736b634b9451189d4f907e43c2
postnik0706 <postnik0706@0245cc7d-1c5a-0d63-4fdc-0e8d0fe1a5ef>
2012-05-15 05:40:35 +0000

    Saving the Wikipedia query - will be deleting it in favor of census data

--- trunk/RandomDataGenerator/RandomDataGenerator/Program.cs
+++ trunk/RandomDataGenerator/RandomDataGenerator/Program.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using HtmlAgilityPack;
+using System.Net;
+using System.IO;
 
 namespace RandomDataGenerator
 {
     class Program
     {
+        private const string CACHE_FILENAME_LAST_NAMES = @"./dist.all.last";
+        private const string CACHE_FILENAME_FIRST_NAMES = @"./dist.all.last";
+
         static void Main(string[] args)
         {
-            HtmlWeb web = new HtmlWeb();
-            HtmlDocument doc = web
+            // Wikipedia - 100 most popular last names
+            /*
+            HtmlWeb wiki = new HtmlWeb();
+            HtmlDocument doc = wiki
                 .Load("http://en.wikipedia.org/wiki/List_of_most_common_surnames_in_North_America");
 
             var tags = doc.DocumentNode.SelectNodes("/html[1]/body[1]/div[3]/div[3]/div[4]/table[4]/tr/td[1]");
@@ -19,6 +25,20 @@ namespace RandomDataGenerator
             foreach (var item in tags)
             {
                 Console.WriteLine(item.InnerText);
+            }
+            */
+ 
+            // Census - most popular US names
+            WebClient census = new WebClient();
+            census.DownloadFile("http://www.census.gov/genealogy/www/data/1990surnames/dist.all.last", CACHE_FILENAME_LAST_NAMES);
+            string[] names = (from n in File.ReadAllLines(CACHE_FILENAME_LAST_NAMES)
+                              select n.Substring(0, 1).ToUpper() + (n.Substring(1, n.IndexOf(" ") - 1)).ToLower()).ToArray();
+            census.DownloadFile("http://www.census.gov/genealogy/www/data/1990surnames/dist.female.first", CACHE_FILENAME
+                              
+                              
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
             }
         }
     }
