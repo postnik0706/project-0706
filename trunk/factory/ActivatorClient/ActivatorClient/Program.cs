7017160b75b059a479f0fd998b3ff6f4786241f7
postnik0706 <postnik0706@0245cc7d-1c5a-0d63-4fdc-0e8d0fe1a5ef>
2012-05-20 05:45:50 +0000

    Added ActivatorClient, tests for ActivatorClient

---
+++ trunk/factory/ActivatorClient/ActivatorClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ActivatorClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if ( (args.Length != 3) || ( (args.Length >= 1) && (args[0] == "/?")) )
+                Utilities.ShowInColor(
+@"Usage parameters:
+/Bldg <Bldg ID> /Activate
+/Bldg <Bldg ID> /Deactivate
+", ConsoleColor.Green);
+
+            foreach (var item in args)
+            {
+                if ( (item == "/Bldg") && (item.Length > item.IndexOf(item)) )
+
+            }
+        }
+    }
+}
---
+++ trunk/factory/ActivatorClient/Test/ActivatorClientTests.dpr
@@ -0,0 +1,35 @@
+program ActivatorClientTests;
+{
+
+  Delphi DUnit Test Project
+  -------------------------
+  This project contains the DUnit test framework and the GUI/Console test runners.
+  Add "CONSOLE_TESTRUNNER" to the conditional defines entry in the project options
+  to use the console test runner.  Otherwise the GUI test runner will be used by
+  default.
+
+}
+
+{$IFDEF CONSOLE_TESTRUNNER}
+{$APPTYPE CONSOLE}
+{$ENDIF}
+
+uses
+  TestUMain in 'TestUMain.pas',
+  UMain in '..\UMain.pas',
+  Forms,
+  TestFramework,
+  GUITestRunner,
+  TextTestRunner;
+
+{R *.RES}
+
+begin
+  Application.Initialize;
+  if IsConsole then
+    with TextTestRunner.RunRegisteredTests do
+      Free
+  else
+    GUITestRunner.RunRegisteredTests;
+end.
+
