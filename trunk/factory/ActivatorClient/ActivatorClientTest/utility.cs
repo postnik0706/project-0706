eab45989b5d405619ce985a4db3121845150d60e
postnik0706 <postnik0706@0245cc7d-1c5a-0d63-4fdc-0e8d0fe1a5ef>
2012-05-21 01:52:36 +0000

    Cleaning up

---
+++ trunk/factory/ActivatorClient/ActivatorClientTest/utility.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ActivatorClient;
+
+namespace ActivatorClientTest
+{
+    [TestClass]
+    public class utility
+    {
+        [TestCleanup]
+        public void Cleanup()
+        {
+            CmdLine.Reset();
+        }
+
+        [TestMethod]
+        public void CommandLineParameters_HelpRequired()
+        {
+            CmdLine.Load( new string[] {"/?"} );
+            Assert.AreEqual(true, CmdLine.Parameters.HelpRequired);
+        }
+
+        [TestMethod]
+        public void CommandLineParameters_All_Set_Up()
+        {
+            CmdLine.Load(new string[] { "/Bldg", "1", "/Activate" });
+            Assert.AreEqual(1, CmdLine.Parameters.BuildingID);
+            Assert.AreEqual(true, CmdLine.Parameters.IsToActivate);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException),
+            "Not all the parameters were set up")]
+        public void CommandLineParameters_LackOfOneMustFail()
+        {
+            CmdLine.Load(new string[] { "/Bldg", "1" });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException),
+            "Not all the parameters were set up")]
+        public void CommandLineParameters_NoAnyParameters()
+        {
+            CmdLine.Load(new string[] { "" });
+        }
+    }
+}
