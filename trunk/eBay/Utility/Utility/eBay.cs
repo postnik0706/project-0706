f0a8fa8a3b6f81183bafa3124fbc3e3639b7cfd4
postnik0706 <postnik0706@0245cc7d-1c5a-0d63-4fdc-0e8d0fe1a5ef>
2012-08-05 00:45:22 +0000

    editions

--- trunk/eBay/Utility/Utility/eBay.cs
+++ trunk/eBay/Utility/Utility/eBay.cs
@@ -12,6 +12,12 @@ namespace Utility
 {
     public class eBay
     {
+        public static void AddLogInfo(string Info)
+        {
+            Debug.WriteLine(
+                String.Format("{0}:\t{1}", DateTime.Now.ToString(), Info));
+        }
+        
         public static ItemType BuildItem()
         {
             ItemType item = new ItemType();
@@ -20,14 +26,14 @@ namespace Utility
             item.Title = "Gadget G100";
             item.Description = "My test gadget";
 
-            item.ListingType = ListingTypeCodeType.Express;
+            item.ListingType = ListingTypeCodeType.Chinese;
 
             // listing price
             item.Currency = CurrencyCodeType.USD;
             item.StartPrice = new AmountType() { Value = 15.99, currencyID = CurrencyCodeType.USD };
 
             // listing duration
-            item.ListingDuration = "Days_255";
+            item.ListingDuration = "Days_3";
 
             // item location and country
             item.Location = "Farmington";
@@ -41,8 +47,9 @@ namespace Utility
 
             // payment methods
             item.PaymentMethods = new BuyerPaymentMethodCodeTypeCollection(
-                new BuyerPaymentMethodCodeType[] { BuyerPaymentMethodCodeType.CashInPerson }
+                new BuyerPaymentMethodCodeType[] { BuyerPaymentMethodCodeType.PayPal }
             );
+            item.PayPalEmailAddress = "me@ebay.com";
 
             // item condition, new
             item.ConditionID = 1000;
@@ -50,12 +57,44 @@ namespace Utility
             // handling time is required
             item.DispatchTimeMax = 5;
 
+            item.ShippingDetails = BuildShippingDetailsType();
+
+            item.ProductListingDetails = new ProductListingDetailsType()
+            {
+                UPC = "0027541000085",      // water
+                ProductDetailsURL = "http://alex.tenzee.com",
+                DetailsURL = "http://alex.tenzee.com"
+            };
+
             return item;
+        }
+
+        public static ShippingDetailsType BuildShippingDetailsType()
+        {
+            ShippingDetailsType sd = new ShippingDetailsType();
+
+            sd.ApplyShippingDiscount = true;
+
+            sd.ShippingType = ShippingTypeCodeType.Flat;
+
+            sd.ShippingServiceOptions = new ShippingServiceOptionsTypeCollection(
+                new ShippingServiceOptionsType[] {
+                    new ShippingServiceOptionsType()
+                    {
+                        ShippingService = ShippingServiceCodeType.ShippingMethodStandard.ToString(),
+                        ShippingServiceAdditionalCost = new AmountType() { Value = 0.01, currencyID = CurrencyCodeType.USD },
+                        ShippingServiceCost = new AmountType() { Value = 1.99, currencyID = CurrencyCodeType.USD },
+                        ShippingServicePriority = 1,
+                        ShippingInsuranceCost = new AmountType() { Value = 5.01, currencyID = CurrencyCodeType.USD }
+                    }
+                }
+                );
+            return sd;
         }
 
         public static void Run()
         {
-            Debug.WriteLine(
+            AddLogInfo(
                 String.Format("{0}:\tStarting...", DateTime.Now.ToString()));
 
             ApiContext apiContext = new ApiContext();
@@ -71,12 +110,34 @@ namespace Utility
             /*******************************************
             Getting server time
             */
+            {
+                GeteBayOfficialTimeCall apiCall = new GeteBayOfficialTimeCall(apiContext);
+                DateTime officialTime = apiCall.GeteBayOfficialTime();
 
-            GeteBayOfficialTimeCall apiCall = new GeteBayOfficialTimeCall(apiContext);
-            DateTime officialTime = apiCall.GeteBayOfficialTime();
+                AddLogInfo(
+                    String.Format("Official eBay time: {0}", officialTime));
+            }
 
-            Debug.WriteLine(
-                String.Format("Official eBay time: {0}", officialTime));
+            /*******************************************
+            Adding an item
+            */
+            ItemType item = BuildItem();
+
+            {
+                AddItemCall apiCall = new AddItemCall(apiContext);
+                AddLogInfo("Beginning to call eBay, please wait...");
+
+                FeeTypeCollection fees = apiCall.AddItem(item);
+                AddLogInfo("Completed the call");
+
+                AddLogInfo("The item was listed successfully");
+                AddLogInfo("*************** BEGINNING OF THE LIST");
+                foreach (FeeType i in fees)
+                {
+                    AddLogInfo(String.Format("{0} = {1}", i.Name, i.Fee));
+                }
+                AddLogInfo("*************** END OF THE LIST");
+            }
         }
     }
 }
