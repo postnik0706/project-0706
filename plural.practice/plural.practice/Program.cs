d42f2e48d8fce2518f8bdb2ab94b29688e353f47
postnik0706@gmail.com <postnik0706@gmail.com@0245cc7d-1c5a-0d63-4fdc-0e8d0fe1a5ef>
2012-08-10 01:18:57 +0000

    Reading XML and getting a nullable attribute

--- plural.practice/plural.practice/Program.cs
+++ plural.practice/plural.practice/Program.cs
@@ -10,6 +10,28 @@ namespace plural.practice
     {
         static void Main(string[] args)
         {
+            CreateSimpleXml();
+            ReadSimpleXml();
+        }
+
+        private static void ReadSimpleXml()
+        {
+            XDocument doc = XDocument.Load("modules.xml");
+
+            XElement root = doc.Root;
+
+            var elements = root.Descendants();
+
+            foreach (var element in elements)
+            {
+                string value = element.Value;
+
+                string length = (string)element.Attribute("Length");
+            }
+        }
+
+        private static void CreateSimpleXml()
+        {
             XDocument doc = new XDocument(
                 new XElement("Modules",
                     new XElement("Module", "Introduction to LINQ"),
