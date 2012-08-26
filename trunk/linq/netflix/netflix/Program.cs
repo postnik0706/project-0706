f8ffd9e663a20a5af9d07ad055bbf2400bafe8e5
postnik0706 <postnik0706@0245cc7d-1c5a-0d63-4fdc-0e8d0fe1a5ef>
2012-08-26 07:01:12 +0000

    Initial check-in

---
+++ trunk/linq/netflix/netflix/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Configuration;
+using System.Web;
+using System.IO;
+using System.Xml.Linq;
+
+namespace netflix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Uri_base = "http://api-public.netflix.com/catalog/titles";
+            
+            Uri uri = new Uri(String.Format("{0}/{1}?oauth_consumer_key={2}&term={3}",
+                Uri_base, "autocomplete",
+                HttpUtility.UrlEncode(ConfigurationManager.AppSettings["consumerKey"]),
+                "a"));
+            /*Console.WriteLine("Host: {0}; LocalPath: {1}; Query: {2}", uri.Host, uri.LocalPath,
+                uri.Query);
+            */
+            WebRequest request = WebRequest.Create(uri);
+            ((HttpWebRequest)request).UserAgent = ".Net";
+            WebResponse response = request.GetResponse();
+            try
+            {
+                Stream stream = response.GetResponseStream();
+                StreamReader sr = new StreamReader(stream);
+                string strResponse = sr.ReadToEnd();
+
+                FileStream f = new FileStream(@".\out.txt", FileMode.OpenOrCreate);
+                StreamWriter w = new StreamWriter(f);
+                w.Write(strResponse);
+                w.Close();
+
+                XDocument doc = new XDocument();
+                doc = XDocument.Parse(strResponse);
+                Console.WriteLine(doc);
+                stream.Close();
+            }
+            finally
+            {
+                response.Close();
+            } 
+        }
+    }
+}
