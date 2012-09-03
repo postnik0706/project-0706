d6bcb601f77f9596cbc26af4cce6b99dc32ba327
postnik0706 <postnik0706@0245cc7d-1c5a-0d63-4fdc-0e8d0fe1a5ef>
2012-09-03 04:19:02 +0000

    Added configuration load

---
+++ trunk/plural.practice/app_log4net/app_log4net/Bar.cs
@@ -0,0 +1,16 @@
+﻿using log4net;
+
+namespace com.foo
+{
+    class Bar
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Bar));
+        
+        internal void DoIt()
+        {
+            log.Debug("Did it again!");
+            log.Warn("And here is a warning");
+            log.Error("And here is an exception");
+        }
+    }
+}
