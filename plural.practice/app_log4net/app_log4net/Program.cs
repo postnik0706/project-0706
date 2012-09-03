39f7085908dc16b84f245634536729ccd800dacb
postnik0706 <postnik0706@0245cc7d-1c5a-0d63-4fdc-0e8d0fe1a5ef>
2012-09-03 04:19:02 +0000

    Added configuration load

---
+++ plural.practice/app_log4net/app_log4net/Program.cs
@@ -0,0 +1,21 @@
+﻿using com.foo;
+using log4net;
+using log4net.Config;
+
+namespace app_log4net
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+        
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure(new System.IO.FileInfo("app_log4net.exe.config"));
+
+            log.Info("Entering app");
+            Bar bar = new Bar();
+            bar.DoIt();
+            log.Info("Exiting app");
+        }
+    }
+}
