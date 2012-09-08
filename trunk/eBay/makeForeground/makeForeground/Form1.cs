dfde8aa5b13e7850e165f9d1606df4c804e4efcd
postnik0706 <postnik0706@0245cc7d-1c5a-0d63-4fdc-0e8d0fe1a5ef>
2012-09-08 21:19:38 +0000

    Initial check-in

---
+++ trunk/eBay/makeForeground/makeForeground/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace makeForeground
+{
+    // http://www.shloemi.com/2012/09/solved-setforegroundwindow-win32-api-not-always-works/
+    
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern int GetWindowTextLength(IntPtr hWnd);
+        /*
+        [DllImport("User32.dll")]
+        public static extern Int32 SetForegroundWindow(int hWnd);
+        [DllImport("User32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+        */
+        
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId); // When you don't want the ProcessId, use this overload and pass IntPtr.Zero for the second parameter
+        [DllImport("user32.dll")]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, IntPtr ProcessId);
+        [DllImport("kernel32.dll")]
+        public static extern uint GetCurrentThreadId();
+        /// <summary>The GetForegroundWindow function returns a handle to the foreground window.</summary>
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll")]
+        public static extern bool AttachThreadInput(uint idAttach, uint idAttachTo, bool fAttach);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool BringWindowToTop(IntPtr hWnd);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool BringWindowToTop(HandleRef hWnd);
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, uint nCmdShow);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static void ForceForegroundWindow(IntPtr hWnd) {
+            uint foreThread = GetWindowThreadProcessId(GetForegroundWindow(), IntPtr.Zero);
+            uint appThread = GetCurrentThreadId();
+            const uint SW_SHOW = 5;
+            if (foreThread != appThread)
+            {
+                AttachThreadInput(foreThread, appThread, true);
+                BringWindowToTop(hWnd);
+                ShowWindow(hWnd, SW_SHOW);
+                AttachThreadInput(foreThread, appThread, false);
+            }
+            else
+            {
+                BringWindowToTop(hWnd);
+                ShowWindow(hWnd, SW_SHOW);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            IntPtr hWnd = GetForegroundWindow();
+            label1.Text = hWnd.ToString();
+            int length = GetWindowTextLength(hWnd);
+            StringBuilder sb = new StringBuilder(length + 1);
+            GetWindowText(hWnd, sb, sb.Capacity);
+            label2.Text = sb.ToString();
+
+            if (sb.ToString().IndexOf("Notepad") > 0)
+            {
+                // First approach
+                //this.Activate();
+
+                // Second approach
+                ForceForegroundWindow(this.Handle);
+            }
+
+        }
+    }
+}
