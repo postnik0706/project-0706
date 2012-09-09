4781f813d58f80f733d0caa02cd753a67881ed93
postnik0706 <postnik0706@0245cc7d-1c5a-0d63-4fdc-0e8d0fe1a5ef>
2012-09-09 10:53:07 +0000

    Added a new test utility

---
+++ trunk/eBay/Utility/eBayTest/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using eBay.Service.Core.Sdk;
+using eBay.Service.Call;
+using Utility;
+using System.IO;
+using System.Configuration;
+
+namespace eBayTest
+{
+    public delegate void RefreshControlEvent(string Contents);
+
+    public partial class Form1 : Form
+    {
+        LogWatcher logWatcher;
+        public RefreshControlEvent OnRefresh;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ApiContext apiCtxSeller = eBayClass.GetContext(eBayClass.SellerOrBuyer.typeSELLER);
+            GeteBayOfficialTimeCall timeCall = new GeteBayOfficialTimeCall(apiCtxSeller);
+            DateTime eBayTime = timeCall.GeteBayOfficialTime();
+            TimeSpan timeDiff = DateTime.Now - eBayTime;
+            edeBayTime.Text = String.Format("{0:F}", eBayTime);
+            eBayClass.Metrics.GenerateReport(eBayClass.LogManager.ApiLoggerList[0]);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OnRefresh = new RefreshControlEvent(RefreshControl);
+            logWatcher = new LogWatcher(ConfigurationManager.AppSettings["eBayLogger"],
+                this, log);
+        }
+
+        private void RefreshControl(string Contents)
+        {
+            if ((log.Text.Length < Contents.Length) && (log.Text.Length != 0))
+            {
+                log.AppendText(Contents.Substring(log.Text.Length));
+            }
+            else
+                log.Text = Contents;
+
+            log.SelectionStart = log.Text.Length;
+            log.SelectionLength = 0;
+            log.ScrollToCaret();
+        }
+        
+        private void btnClearLog_Click(object sender, EventArgs e)
+        {
+            logWatcher.Pause();
+            try
+            {
+                File.WriteAllText(ConfigurationManager.AppSettings["eBayLogger"], "");
+            }
+            finally
+            {
+                logWatcher.Resume();
+            }
+        }
+    }
+}
---
+++ trunk/eBay/Utility/eBayTest/LogWatcher.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace eBayTest
+{
+    public class LogWatcher
+    {
+        FileStream Stream;
+        StreamReader Reader;
+        FileSystemWatcher Watcher;
+
+        public string Filename { get; set; }
+        public RichTextBox Control { get; set; }
+        public Form1 Parent { get; set; }
+
+        public LogWatcher(string Filename, Form1 Parent, RichTextBox Control)
+        {
+            this.Filename = Filename;
+            this.Control = Control;
+            this.Parent = Parent;
+
+            RefreshControl();
+            Watcher = new FileSystemWatcher();
+            Watcher.Path = Path.GetDirectoryName(Filename);
+            Watcher.NotifyFilter = (NotifyFilters.LastWrite | NotifyFilters.Size);
+            Watcher.Filter = Path.GetFileName(Filename);
+            Watcher.Changed += OnChanged;
+            Watcher.EnableRaisingEvents = true;
+        }
+
+        private void RefreshControl()
+        {
+            string Contents;
+
+            using (Stream = new System.IO.FileStream(Filename, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                using (Reader = new System.IO.StreamReader(Stream))
+                {
+                    Contents = Reader.ReadToEnd();
+                }
+            }
+
+            Parent.Invoke(Parent.OnRefresh, Contents);
+        }
+        
+        public void OnChanged(object o, FileSystemEventArgs e)
+        {
+            RefreshControl();
+        }
+
+        public void Pause()
+        {
+            Watcher.EnableRaisingEvents = false;
+        }
+        
+        public void Resume()
+        {
+            RefreshControl();
+            Watcher.EnableRaisingEvents = true;
+        }
+    }
+}
