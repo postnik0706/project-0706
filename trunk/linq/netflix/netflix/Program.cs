f566cb3daaf5dbf9b903ed0b5da1e2e305ce2920
postnik0706 <postnik0706@0245cc7d-1c5a-0d63-4fdc-0e8d0fe1a5ef>
2012-09-20 04:04:41 +0000

    Added the test project

--- trunk/linq/netflix/netflix/Program.cs
+++ trunk/linq/netflix/netflix/Program.cs
@@ -14,38 +14,7 @@ namespace netflix
     {
         static void Main(string[] args)
         {
-            string Uri_base = "http://api-public.netflix.com/catalog/titles";
-            
-            Uri uri = new Uri(String.Format("{0}/{1}?oauth_consumer_key={2}&term={3}",
-                Uri_base, "autocomplete",
-                HttpUtility.UrlEncode(ConfigurationManager.AppSettings["consumerKey"]),
-                "a"));
-            /*Console.WriteLine("Host: {0}; LocalPath: {1}; Query: {2}", uri.Host, uri.LocalPath,
-                uri.Query);
-            */
-            WebRequest request = WebRequest.Create(uri);
-            ((HttpWebRequest)request).UserAgent = ".Net";
-            WebResponse response = request.GetResponse();
-            try
-            {
-                Stream stream = response.GetResponseStream();
-                StreamReader sr = new StreamReader(stream);
-                string strResponse = sr.ReadToEnd();
 
-                FileStream f = new FileStream(@".\out.txt", FileMode.OpenOrCreate);
-                StreamWriter w = new StreamWriter(f);
-                w.Write(strResponse);
-                w.Close();
-
-                XDocument doc = new XDocument();
-                doc = XDocument.Parse(strResponse);
-                Console.WriteLine(doc);
-                stream.Close();
-            }
-            finally
-            {
-                response.Close();
-            } 
         }
     }
 }
