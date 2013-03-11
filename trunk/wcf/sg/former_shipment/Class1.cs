23ba144977be89b2e26ea806d256bcc6e5ca4f8f
postnik0706 <postnik0706@0245cc7d-1c5a-0d63-4fdc-0e8d0fe1a5ef>
2013-03-11 02:17:07 +0000

    initial check-in

---
+++ trunk/wcf/sg/former_shipment/Class1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace OldShipment
+{
+    public enum WeightType { Unknown, LBS, KG };
+    
+    public class ShipmentInfo
+    {
+        [XmlAttribute]
+        public string SG_ID { get; set; }
+        [XmlAttribute(AttributeName = "Voided")]
+        public string voided { get; set; }
+        [XmlIgnore]
+        public bool Voided { get { return bool.Parse(voided); } set { voided = value.ToString(); } }
+        [XmlAttribute]
+        public string LastAction { get; set; }
+    }
+
+    public class Package
+    {
+        [XmlAttribute(AttributeName = "SG_ID")]
+        public string SG_ID { get; set; }
+        [XmlAttribute(AttributeName = "Voided")]
+        public string voided { get; set; }
+        [XmlIgnore]
+        public bool Voided { get { return bool.Parse(voided); } set { voided = value.ToString(); } }
+        [XmlAttribute]
+        public string LastAction { get; set; }
+        [XmlAttribute]
+        public string Carrier { get; set; }
+        [XmlAttribute]
+        public string TrackingNumber { get; set; }
+        [XmlAttribute]
+        public string PackageType { get; set; }
+        [XmlAttribute]
+        public decimal PackageWeight { get; set; }
+        [XmlAttribute(AttributeName = "PackageWeightUM")]
+        public string packageWeightUM { get; set; }
+        [XmlIgnore]
+        public WeightType PackageWeightUM { get { return (WeightType)Enum.Parse(typeof(WeightType), packageWeightUM); } set { packageWeightUM = value.ToString(); } }
+        [XmlAttribute]
+        public string Reference { get; set; }
+        [XmlAttribute]
+        public decimal DeclaredValue { get; set; }
+        [XmlAttribute]
+        public decimal DimHeight { get; set; }
+        [XmlAttribute]
+        public decimal DimWidth { get; set; }
+        [XmlAttribute]
+        public decimal DimLength { get; set; }
+        [XmlAttribute]
+        public string weightUOM { get; set; }
+        [XmlIgnore]
+        public WeightType WeightUOM { get { return (WeightType)Enum.Parse(typeof(WeightType), weightUOM); } set { weightUOM = value.ToString(); } }
+        [XmlAttribute]
+        public string dimUOM { get; set; }
+
+    }
+
+    [XmlRoot(ElementName="SHIPGEARHISTORY")]
+    public class ShipGearHistory
+    {
+        [XmlAttribute]
+        public string ShipmentID { get; set; }
+        [XmlAttribute]
+        public string LastAction { get; set; }
+        [XmlAttribute]
+        public string SGID { get; set; }
+        [XmlAttribute(AttributeName = "VoidFlag")]
+        public string voidFlag { get; set; }
+        [XmlIgnore]
+        public bool VoidFlag { get { return bool.Parse(voidFlag.ToLower()); } set { voidFlag = value.ToString(); } }
+        [XmlAttribute]
+        public int FSI_ID { get; set; }
+        [XmlAttribute]
+        public int CompanyID { get; set; }
+        [XmlAttribute]
+        public string Document { get; set; }
+        [XmlAttribute]
+        public string DocumentKey { get; set; }
+        [XmlAttribute]
+        public string DocumentKey2 { get; set; }
+        [XmlAttribute]
+        public int NumberOfPackages { get; set; }
+        [XmlAttribute]
+        public string RateRuleApplied { get; set; }
+        [XmlAttribute]
+        public decimal FreightCharges { get; set; }
+        /*[XmlIgnore]
+        public decimal FreightCharges { get { return decimal.Parse(freightCharges); } set { freightCharges = value.ToString(); } } */
+        [XmlAttribute(AttributeName = "ShipDate")]
+        public string shipDate { get; set; }
+        [XmlIgnore]
+        public DateTime ShipDate { get { return DateTime.Parse(shipDate); } set { shipDate = value.ToString(); } }
+        [XmlAttribute]
+        public string CustomerName { get; set; }
+        [XmlAttribute]
+        public string FSCompanyName { get; set; }
+        [XmlAttribute]
+        public string Workstation { get; set; }
+        [XmlAttribute(AttributeName = "AddedNotes")]
+        public string addedNotes { get; set; }
+        [XmlIgnore]
+        public bool AddedNotes { get { return bool.Parse(addedNotes); } set { addedNotes = value.ToString(); } }
+        [XmlAttribute(AttributeName = "AddedFreight")]
+        public string addedFreight { get; set; }
+        [XmlIgnore]
+        public bool AddedFreight { get { return bool.Parse(addedFreight); } set { addedFreight = value.ToString(); } }
+        [XmlAttribute]
+        public string NotesText { get; set; }
+        [XmlAttribute]
+        public string Carrier { get; set; }
+        [XmlAttribute]
+        public string Service { get; set; }
+        [XmlAttribute]
+        public string Reference { get; set; }
+
+        [XmlElement(ElementName = "SHIPMENTINFO")]
+        public ShipmentInfo ShipmentInfo { get; set; }
+
+        [XmlElement(ElementName = "PACKAGE")]
+        public Package Package { get; set; }
+    }
+}
---
+++ trunk/wcf/sg/tests/xml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml.Serialization;
+using OldShipment;
+using System.IO;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace tests
+{
+    internal class Mother
+    {
+        public static ShipGearHistory GetTestHistoryRecord()
+        {
+            return new ShipGearHistory()
+            {
+
+            };
+        }
+    }
+    
+    [TestClass]
+    public class xml
+    {
+        [TestMethod]
+        public void SaveXml()
+        {
+            XmlSerializer s = new XmlSerializer(typeof(ShipGearHistory), "");
+            ShipGearHistory h = Mother.GetTestHistoryRecord();
+
+            StringBuilder sb = new StringBuilder();
+            using (TextWriter tr = new StringWriter(sb))
+            {
+                s.Serialize(tr, h);
+            }
+            Debug.WriteLine(sb.ToString());
+        }
+
+        [TestMethod]
+        public void LoadXml()
+        {
+            XmlSerializer s = new XmlSerializer(typeof(ShipGearHistory), "");
+
+            Stream sb = Assembly.GetExecutingAssembly().GetManifestResourceStream("tests.xml.ShipGearHistory.xml");
+
+            ShipGearHistory h = (ShipGearHistory)s.Deserialize(sb);
+
+            Assert.IsNotNull(h);
+            Assert.AreEqual("1ZT788870200001193", h.ShipmentID);
+            Assert.AreEqual("Create", h.LastAction);
+            Assert.AreEqual("SG90101O_0001174", h.SGID);
+            Assert.AreEqual(false, h.VoidFlag);
+            Assert.AreEqual(90, h.FSI_ID);
+            Assert.AreEqual(101, h.CompanyID);
+            Assert.AreEqual("Sales Orders", h.Document);
+            Assert.AreEqual("0001174", h.DocumentKey);
+            Assert.AreEqual("", h.DocumentKey2);
+            Assert.AreEqual(1, h.NumberOfPackages);
+            Assert.AreEqual("", h.RateRuleApplied);
+            Assert.AreEqual(65.95M, h.FreightCharges);
+            Assert.AreEqual(new DateTime(2013, 2, 14, 0, 1, 2), h.ShipDate);
+            Assert.AreEqual("Irvine Warehouse", h.CustomerName);
+            Assert.AreEqual("(ABC) ABC Distribution and Service Corp.", h.FSCompanyName);
+            Assert.AreEqual("DEV08", h.Workstation);
+            Assert.AreEqual(true, h.AddedNotes);
+            Assert.AreEqual(false, h.AddedFreight);
+            Assert.AreEqual("this is a test", h.NotesText);
+            Assert.AreEqual("UPS", h.Carrier);
+            Assert.AreEqual("UPS2ndDayAir", h.Service);
+            Assert.AreEqual("0001174", h.Reference);
+
+            Assert.AreEqual("123", h.ShipmentInfo.SG_ID);
+            Assert.AreEqual(true, h.ShipmentInfo.Voided);
+        }
+    }
+}
